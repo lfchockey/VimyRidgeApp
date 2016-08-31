431f29d4b0e7e2f70b2d45f8988af8c3d88b2365
lfchockey <Blacky@masons-imac.local>
2016-08-31 07:35:04 -0400

    Committing 5 files (not sure why they weren't a part of the original)

---
+++ Vimy Ridge Project/MedalReader.swift
@@ -0,0 +1,51 @@
+//
+//  Coordinates.swift
+//  MapsTutorial
+//
+//  Created by Student on 2016-01-13.
+//  Copyright © 2016 SFDCI. All rights reserved.
+//
+
+import Foundation
+
+class MedalReader
+{
+    var medal: Medal
+
+    var name: String?
+    
+    init(medalName: String)
+    {
+        let filePath = NSBundle.mainBundle().pathForResource("Medals List", ofType: "plist")
+        let properties = NSDictionary(contentsOfFile: filePath!)
+        
+        let individualMedal = properties![medalName] as! NSDictionary
+
+        let medalWebsite = individualMedal["website"]
+        
+        let medalDescription = individualMedal["medalDescription"]
+        
+        let imageName = individualMedal["imageName"]
+        
+        medal = Medal(newWebsite: medalWebsite! as! String, newMedalDescription: medalDescription! as! String, newImageName: imageName! as! String)
+        name = medalName
+
+        
+    }
+}
+
+
+class Medal
+{
+    
+    var website: String
+    var medalDescription: String
+    var imageName: String
+    init(newWebsite: String, newMedalDescription: String, newImageName: String)
+    {
+        website = newWebsite
+        medalDescription = newMedalDescription
+        imageName = newImageName
+    }
+    
+}
