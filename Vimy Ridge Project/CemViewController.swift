a4516b74215af393bba46f761e5a599fdf346cba
Student <student@SFDCIs-iMac-5.local>
2016-12-20 12:51:41 -0500

    first day of committing

--- Vimy Ridge Project/CemViewController.swift
+++ Vimy Ridge Project/CemViewController.swift
@@ -12,6 +12,10 @@ import MapKit
 class CemViewController: UIViewController, MKMapViewDelegate{
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var region: UILabel!
+    @IBOutlet weak var casualties: UILabel!
+    @IBOutlet weak var histInfo: UITextView!
+    @IBOutlet weak var visitInfo: UITextView!
     
     var regionRadius: CLLocationDistance = 10000
 
@@ -22,6 +26,12 @@ class CemViewController: UIViewController, MKMapViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = ("Title: \(MapVariables.cemeteryTitle)")
+        
+        nameLabel.text = "Tap A Cemetery"
+        casualties.text = ""
+        region.text = ""
+        histInfo.text = ""
+        visitInfo.text = ""
     }
 }
 
