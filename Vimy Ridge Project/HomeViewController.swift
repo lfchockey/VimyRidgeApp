517cd956ad7857e69fef79942444992fa95f5e46
lfchockey <sfdci@SFDCIs-MacBook-Pro-4.local>
2016-12-20 07:51:48 -0500

    Master Copy prior to submitting

--- Vimy Ridge Project/HomeViewController.swift
+++ Vimy Ridge Project/HomeViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mason Black on 2015-07-09.
 //  Copyright (c) 2015 Mason Black. All rights reserved.
 //
+// Culminating Assignment Student Branches
 
 import UIKit
 import AVFoundation
